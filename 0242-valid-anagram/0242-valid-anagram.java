671d8a10a383dc77d90915cb8c1bbaf89feaebdd
Sai Chandrakanth Bobbili <154310703+saichandrakant@users.noreply.github.com>
2026-09-16 09:36:56 +0530

    Time: 5 ms (63.08%), Space: 44.3 MB (81.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] hash=new int[26];
+        if(s.length() !=t.length())
+        {
+            return false;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            hash[s.charAt(i)-'a']++;
+            hash[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(hash[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
